b7e81b9ddc9498aa9d5cabc805b78ca064183871
YANG HYEON BIN <iks05280204@gmail.com>
2025-10-04 21:16:37 +0900

    gunman 캐릭터 총알 발사

--- gamemanager.py
+++ gamemanager.py
@@ -2,17 +2,39 @@ from pico2d import *
 from object import *
 from paint import *
 
-attack_end = False
-attack_end_timer = 0.0
-gunman = Character([[load_image('hope01_01.png'), load_image('hope01_02.png')],
-                        [load_image('hope01_03.png'), load_image('hope01_04.png')],
-                        [load_image('hope01_05.png'), load_image('hope01_06.png'), load_image('hope01_07.png')]],
-                       600, 400)
+WIDTH = 1200
+HEIGHT = 800
+open_canvas(WIDTH, HEIGHT)
+
+gunman = Character([
+    [load_image(f'source\\hope01_0{i}.png') for i in range(1, 3)],
+    [load_image(f'source\\hope01_0{i}.png') for i in range(3, 5)],
+    [load_image(f'source\\hope01_0{i}.png') for i in range(5, 8)]
+], WIDTH / 2, HEIGHT / 2)  # 컴프리헨션 사용
+bullet = []
+
+prev_time = get_time()
+
+
+def DeltaTime():
+    global prev_time
+    curr_time = get_time()
+    dt = curr_time - prev_time
+    prev_time = curr_time
+    return dt
 
 
 def GameUpdate(dt):
-    global attack_end, attack_end_timer
+    global bullet
+    InputKey()
+    WaitForShoot(dt)
+    for b in bullet[::-1]:
+        b.update(dt)
+        if not b.visible:
+            bullet.remove(b)
 
+
+def InputKey():
     events = get_events()
     if not gunman.state == "attack":
         for event in events:
@@ -33,24 +55,33 @@ def GameUpdate(dt):
                 if event.key == SDLK_LEFT and gunman.flip == True or event.key == SDLK_RIGHT and gunman.flip == False:
                     gunman.state = "idle"
 
-    if gunman.state == "attack" and gunman.frame == len(gunman.anime[2]) - 1:
-        attack_end = True
 
-    if attack_end:
-        attack_end_timer += dt
-        if attack_end_timer >= 0.5:
-            attack_end_timer = 0.0
+shootMotionEnd = False
+shootMotionEndTimer = 0.0
+
+
+def WaitForShoot(dt):
+    global shootMotionEnd, shootMotionEndTimer
+
+    if gunman.state == "attack" and gunman.frame == len(gunman.anime[2]) - 1:
+        shootMotionEnd = True
+
+    if shootMotionEnd:
+        shootMotionEndTimer += dt
+        if shootMotionEndTimer >= 0.1:
+            shootMotionEndTimer = 0.0
             gunman.frame = 0
             gunman.state = "idle"
-            attack_end = False
+            shootMotionEnd = False
+            Shoot()
 
-prev_time = get_time()
-def DeltaTime():
-    global prev_time
-    curr_time = get_time()
-    dt = curr_time - prev_time
-    prev_time = curr_time
-    return dt
+
+def Shoot():
+    global bullet
+    bullet.append(Projectile([load_image(f'source\\40241_s2_0{i}.png') for i in range(1, 5)],
+                             gunman.x + 100 - (200 * (int)(gunman.flip)), gunman.y - 30, 122, 66,
+                             500, 0, 0.0, 0.5, gunman.flip, True))
+
 
 def main():
     while (True):
