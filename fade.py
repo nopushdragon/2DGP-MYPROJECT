112520f936bf2e9d9c62437ec0d3ff072b0e8a12
AN MIN YONG <iks05280204@gmail.com>
2025-10-28 19:39:54 +0900

    fade in/out 완성

--- fade.py
+++ fade.py
@@ -2,16 +2,17 @@ from pico2d import *
 from stages import Stages
 import gamemanager
 
-black_fade_image = load_image('source\\background\\black.png')
+black_fade_image = load_image('source\\background\\fade_black.png')
+#black_fade_image = load_image("source\\skill_icon\\asha\\hero_illust_09_Asha.png")
 
 STATE_IDLE = 0
 STATE_FADING_OUT = 1
 STATE_FADING_IN = 2
 
-FADE_SPEED = 0.05
+FADE_SPEED = 0.02
 
 fade_state = STATE_IDLE  # 현재 페이드 상태
-fade_alpha = 0.0         # 현재 투명도 (0.0 ~ 1.0)
+fade_alpha = 0.0        # 현재 알파 값
 
 next_scene = ""
 
@@ -30,7 +31,7 @@ def update():
     if not fade_state == STATE_IDLE:
         if fade_state == STATE_FADING_OUT:
             fade_alpha += FADE_SPEED
-            if fade_alpha > 1.0:
+            if fade_alpha >= 1.0:
                 fade_alpha = 1.0
 
                 if "stage" in next_scene:
@@ -49,5 +50,7 @@ def update():
 def draw():
     if fade_state != STATE_IDLE:
         black_fade_image.opacify(fade_alpha)
+        print(fade_alpha)
         # clip_draw 사용하여 전체 화면에 그립니다.
-        black_fade_image.clip_draw(0, 0, 1200, 800, gamemanager.WIDTH // 2, gamemanager.HEIGHT // 2)
+        black_fade_image.clip_draw(0, 0, 120, 80, gamemanager.WIDTH // 2, gamemanager.HEIGHT // 2,1200,800)
+
