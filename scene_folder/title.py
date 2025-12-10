7b5a0101d34c82d4e8686d0e360a155391236a0b
AN MIN YONG <iks05280204@gmail.com>
2025-12-10 14:16:35 +0900

    안전한 종료

--- scene_folder/title.py
+++ scene_folder/title.py
@@ -1,8 +1,8 @@
-
 from gamemanager import WIDTH, HEIGHT
 from pico2d import *
 import fade
 import sound
+import sys
 
 titleBackground = load_image('source\\background\\ai_start.png')
 startButton = load_image('source\\background\\button1.png')
@@ -17,9 +17,15 @@ def Update(dt):
         timer = 0.0
         start_twinkle = not start_twinkle
     handle_events()
+
 def handle_events():
     events = get_events()
     for event in events:
+        # Handle window close
+        if event.type == SDL_QUIT:
+            close_canvas()
+            sys.exit()
+
         if event.type == SDL_MOUSEBUTTONDOWN:
             mx, my = event.x, HEIGHT - event.y
             if( WIDTH / 2 - 200 <= mx <= WIDTH//2 + 200 and HEIGHT / 2 - 325 <= my <= HEIGHT//2 - 275):
@@ -27,7 +33,7 @@ def handle_events():
                 fade.fade_out("home")
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             close_canvas()
-            quit()
+            sys.exit()
 
 def Draw():
     titleBackground.clip_draw(0, 0, 1024, 1024, WIDTH / 2, HEIGHT / 2,1200,1000)
